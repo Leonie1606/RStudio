f8db35e545e68786ed6802817189423be4b686f8
Leonie1606 <leonie.ehrle@gmx.de>
2025-05-07 22:45:58 +0200

    Add files via upload

---
+++ Kapitel 1/R22_1.3_Vektoren/R13_Vektoren_1.R
@@ -0,0 +1,37 @@
+codeoceanR::rt_score()
+
+# A1 ----
+# Füge folgende Zahlen zu einem Vektor zusammen: 8, -5.5, 27, 110
+vektorZahlen <- c(8, -5.5, 27, 110)
+
+
+# A2 ----
+# Wiederhole die Zahl 5 77 mal.
+wiederholteZahl <- rep (5, times=77)
+
+# A3 ----
+# Erstelle eine Sequenz von 500 bis 1 in Einserschritten.
+# CodeGolf Pro Tipp: Diese Aufgabe ist mit 5 Symbolen lösbar.
+zahlenSequenz1 <- 500:1
+
+
+# A4 ----
+# Erstelle eine Sequenz von 500 bis 0 in Abständen von 25.
+zahlenSequenz25 <-seq(from=500, to=0, by=-25) 
+
+
+# A5 ----
+# Erstelle eine Sequenz von 500 bis 0 mit 30 Zahlen (in je gleichem Abstand).
+zahlenSequenz30 <- seq(from=500, to=0, len=30)
+
+
+# A6 ----
+zahlenSequenz31 <- c(zahlenSequenz30, -5) # Diesen Befehl einfach so stehen lassen.
+
+# Füge mittels einer neu zu erstellenden Zuweisung der 'zahlenSequenz31' am Ende die Zahl -5 hinzu.
+# Die letzte Zahl (an Position 31) soll also -5 sein.
+# Es gibt 3 verschiedene Methoden dafür, die Auswahl ist dir überlassen :).
+
+
+
+# Mache weiter in "R13_Vektoren_2.R"
